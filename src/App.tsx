fe044e89c04154dc4bf57fb1aaa344128b550051
Jihye Kim <wlgp115@gmail.com>
2024-05-29 15:53:58 +0900

    0일 때는 초기화 버튼 없게

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  // 0일 때 초기화 버튼 안 보이게
+  let reset = (
+    <button
+      onClick={() => {
+        console.log("클릭 감지됨");
+        setCount(0);
+      }}
+    >
+      초기화
+    </button>
+  );
+  if (count === 0) {
+    reset = <button>❤</button>;
+  }
+
   return (
     <div>
       <h1>{count}</h1>
@@ -15,14 +31,7 @@ function App() {
         - 1
       </button>
       <span> </span>
-      <button
-        onClick={() => {
-          console.log("클릭 감지됨");
-          setCount(0);
-        }}
-      >
-        초기화
-      </button>
+      {reset}
       <span> </span>
       <button
         onClick={() => {
@@ -31,7 +40,7 @@ function App() {
         }}
       >
         + 1
-      </button>      
+      </button>
     </div>
   );
 }
