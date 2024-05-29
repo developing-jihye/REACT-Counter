30077c7a3787bf74fb5b8e0293d9359b8979658b
Jihye Kim <wlgp115@gmail.com>
2024-05-29 12:42:46 +0900

    onclick 이벤트 추가(콘솔 출력만)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div>
-    <h1>0</h1>
-    <button>1 증가</button>
-  </div>;
+  return (
+    <div>
+      <h1>0</h1>
+      <button onClick={() => console.log("클릭 감지됨")}>1 증가</button>
+    </div>
+  );
 }
 
 export default App;
